20296d6bf6e30ba3dc7839501077f69ee1881ebb
nikolozi2001 <mr.qachibaia44@gmail.com>
2024-08-31 02:29:15 +0400

    Creating an Inline editing feature

--- src/components/tasks/Task.vue
+++ src/components/tasks/Task.vue
@@ -11,29 +11,27 @@
         class="ms-2 flex-grow-1"
         :class="completedClass"
         title="Double click the text to edit or remove"
+        @dbclick="isEdit = true"
       >
-        <!-- <div class="relative">
-                                            <input class="editable-task" type="text" />
-                                        </div> -->
-        <span>{{ task.name }}</span>
+        <div class="relative" v-if="isEdit">
+          <input
+            class="editable-task"
+            type="text"
+            @keyup.esc="isEdit = false"
+            v-focus
+          />
+        </div>
+        <span v-else>{{ task.name }}</span>
       </div>
       <!-- <div class="task-date">24 Feb 12:00</div> -->
     </div>
-    <div class="task-actions">
-      <button class="btn btn-sm btn-circle btn-outline-secondary me-1">
-        <iconPencil />
-      </button>
-      <button class="btn btn-sm btn-circle btn-outline-danger">
-        <iconTrash />
-      </button>
-    </div>
+    <TaskActions @edit="isEdit = true" v-show="!isEdit" />
   </li>
 </template>
 
 <script setup>
-import { computed } from "vue";
-import iconPencil from "../icons/iconPencil.vue";
-import IconTrash from "../icons/IconTrash.vue";
+import { computed, ref } from "vue";
+import TaskActions from "./TaskActions.vue";
 
 const props = defineProps({
   task: {
@@ -42,7 +40,13 @@ const props = defineProps({
   },
 });
 
+const isEdit = ref(false);
+
 const completedClass = computed(() =>
   props.task.is_completed ? "completed" : ""
 );
+
+const vFocus = {
+  mounted: (el) => el.focus(),
+};
 </script>
---
+++ src/components/tasks/TaskActions.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="task-actions">
+    <button
+      class="btn btn-sm btn-circle btn-outline-secondary me-1"
+      @click="emit('edit')"
+    >
+      <iconPencil />
+    </button>
+    <button
+      class="btn btn-sm btn-circle btn-outline-danger"
+      @click="emit('remove')"
+    >
+      <iconTrash />
+    </button>
+  </div>
+</template>
+
+<script setup>
+import iconPencil from "../icons/iconPencil.vue";
+import IconTrash from "../icons/IconTrash.vue";
+
+const emit = defineEmits(["edit", "remove"]);
+</script>

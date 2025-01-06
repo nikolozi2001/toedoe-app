2053ce4f98e16cfb0268ea289bdf0517772acbb1
nikolozi2001 <mr.qachibaia44@gmail.com>
2025-01-06 19:47:42 +0400

    79. Setting up Client Authentication

---
+++ src/http/auth-api.js
@@ -0,0 +1,9 @@
+import api from "./api";
+
+export const csfrToken = () => api.get("/sanctum/csrf-cookie");
+
+export const login = (credentials) => api.post("/auth/login", credentials);
+export const register = (user) => api.post("/auth/register", user);
+export const logout = () => api.post("/auth/logout");
+
+export const getUser = () => api.get("/api/user");
